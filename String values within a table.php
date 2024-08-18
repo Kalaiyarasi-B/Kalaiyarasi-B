433d5d6dfeef0ee3fbe6403f3b5b28307504b040
Kalaiyarasi-B <kalaiyarasib2004@gmail.com>
2024-08-18 19:25:18 +0530

    Update String values within a table.php

--- String values within a table.php
+++ String values within a table.php
@@ -4,16 +4,16 @@
 <?php 
   
 $var1 = "Kalaiyarasi B"; 
-$var2 = "BSC Software Systems"; 
+$var2 = "BSC"; 
    
 
 echo "<table border=1 cellspacing=1 cellpadding=1> 
     <tr>  
        <td><font color=green>$var1</font></td> 
-       <td>is best learning platform</td> 
+       <td>is a web developer </td> 
     </tr> 
     <tr>  
-        <td><font color=green>Written by</font></td> 
+        <td><font color=green> Software Systems</font></td> 
         <td>$var2</td> 
     </tr> 
 </table>"; 
