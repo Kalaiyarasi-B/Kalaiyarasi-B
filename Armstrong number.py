a9216f0f4f012cf7ab3400a06fd548f83111ec07
Kalaiyarasi-B <kalaiyarasib2004@gmail.com>
2024-08-18 16:02:11 +0530

    Update

---
+++ Armstrong number.py
@@ -0,0 +1,14 @@
+#Armstrong number program in python 
+
+n = 153  
+s = n  
+b = len(str(n))
+sum1 = 0
+while n != 0:
+    r = n % 10
+    sum1 = sum1+(r**b)
+    n = n//10
+if s == sum1:
+    print("The given number", s, "is armstrong number")
+else:
+    print("The given number", s, "is not armstrong number") 
