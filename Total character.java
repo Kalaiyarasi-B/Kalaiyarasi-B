28cbdc3819a97348d26def544559a9154ddeea17
Kalaiyarasi-B <kalaiyarasib2004@gmail.com>
2024-08-18 20:23:07 +0530

    Update

---
+++ Total character.java
@@ -0,0 +1,19 @@
+//To calculate total characters in string
+
+
+public class CountCharacter    
+{    
+    public static void main(String[] args) {    
+        String string = "The best of both worlds";    
+        int count = 0;    
+            
+        
+        for(int i = 0; i < string.length(); i++) {    
+            if(string.charAt(i) != ' ')    
+                count++;    
+        }    
+            
+         
+        System.out.println("Total number of characters in a string: " + count);    
+    }    
+}     
