1c87f9fa43c4314026683f45e44ef7fd6ad7eb1e
Kalaiyarasi-B <kalaiyarasib2004@gmail.com>
2024-08-18 20:11:43 +0530

    Update

---
+++ switch case.php
@@ -0,0 +1,16 @@
+<?php      
+$num=20;      
+switch($num){      
+case 10:      
+echo("number is equals to 10");      
+break;      
+case 20:      
+echo("number is equal to 20");      
+break;      
+case 30:      
+echo("number is equal to 30");      
+break;      
+default:      
+echo("number is not equal to 10, 20 or 30");      
+}     
+?>  
