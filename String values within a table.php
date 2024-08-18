db3c94ab025bf1eaa218b8ff53b6f160f6355dd6
Kalaiyarasi-B <kalaiyarasib2004@gmail.com>
2024-08-18 18:23:24 +0530

    Update

---
+++ String values within a table.php
@@ -0,0 +1,20 @@
+//PHP program to Display a String values within a table 
+
+
+<?php 
+  
+$var1 = "Kalaiyarasi B"; 
+$var2 = "BSC Software Systems"; 
+   
+
+echo "<table border=1 cellspacing=1 cellpadding=1> 
+    <tr>  
+       <td><font color=green>$var1</font></td> 
+       <td>is best learning platform</td> 
+    </tr> 
+    <tr>  
+        <td><font color=green>Written by</font></td> 
+        <td>$var2</td> 
+    </tr> 
+</table>"; 
+?> 
